4f897e7e78b6c4dd65d54941c5aead1a214973bc
Adebiyi07 <adebiyijohn07@gmail.com>
2022-12-19 23:43:27 +0100

    refactoring

--- forkify/dist/index.62406edb.js
+++ forkify/dist/index.62406edb.js
@@ -1,3 +1,8 @@
+// import icons from "url:../img/icons.svg";
+// import recipeView from '/.views/recipeView.js';
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
+// import * as model from "./model.js";
 const recipeContainer = document.querySelector(".recipe");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
@@ -6,5 +11,27 @@ const timeout = function(s) {
         }, s * 1000);
     });
 };
+//////////////////////////////////////
+// https://forkify-api.herokuapp.com/v2
+const controlRecipes = async function() {
+    try {
+        const id = window.location.hash.slice(1);
+        console.log(id);
+        if (!id) return;
+        recipeView.renderSpinner();
+        //Loading recipe
+        await model.loadRecipe(id);
+        // const { recipe } = model.state;
+        // (2) Rendering the recipe
+        recipeView.render(model.state.recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+[
+    "hashchange",
+    "load"
+].forEach((ev)=>window.addEventListener(ev, controlRecipes)); // window.addEventListener("hashchange", controlRecipes);
+ // window.addEventListener("load".controlRecipes);
 
 //# sourceMappingURL=index.62406edb.js.map
